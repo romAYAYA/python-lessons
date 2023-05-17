7cc23b6d95063c357b10b0c951c2c994b0c53d0a
Roman Khabayev <supernaturalmlady@gmail.com>
2023-05-17 21:33:53 +0600

    'and' 'or' examples added

---
+++ _6_classworks/_2_lesson/_3_task.py
@@ -0,0 +1,14 @@
+val1 = 12
+val2 = 13
+
+if (val1 < 100) and (val2 < 100 or val2 > 100):
+    print('Values are okay')
+else:
+    print('Values are not okay')
+
+val4 = False
+
+if val4:
+    print('its okay')
+else:
+    print('its not okay')
