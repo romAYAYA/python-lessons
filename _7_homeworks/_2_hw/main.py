8d014e14debea36c1c7986c2b123b992dd16ea4a
Roman Khabayev <supernaturalmlady@gmail.com>
2023-05-19 18:07:26 +0600

    hw 2 almost done. 1 task left

---
+++ _7_homeworks/_2_hw/main.py
@@ -0,0 +1,25 @@
+# TODO 1 task
+
+# TODO 2 task
+
+# input_num = int(input('Enter a number: '))
+#
+# if input_num % 2 == 0:
+#     print('This is an even number')
+# else:
+#     print('This is an odd number')
+
+# TODO 3 task
+
+# input_num_a = int(input('Enter first number: '))
+# input_num_b = int(input('Enter second number: '))
+# input_num_c = int(input('Enter third number: '))
+#
+# print((input_num_a + input_num_b) > input_num_c)
+
+# TODO 4 task
+
+# input_num_a = int(input('Enter first number: '))
+# input_num_b = int(input('Enter second number: '))
+#
+# print(input_num_a > input_num_b)
