15c2d2ad567e1bce6508bfcca0a782a74ad9801a
Roman Khabayev <supernaturalmlady@gmail.com>
2023-05-21 18:26:40 +0600

    1st task finished. hw done

--- _7_homeworks/_2_hw/main.py
+++ _7_homeworks/_2_hw/main.py
@@ -1,5 +1,14 @@
 # TODO 1 task
 
+# input_num_a = int(input('Enter first side of a triangle: '))
+# input_num_b = int(input('Enter second side of a triangle: '))
+# input_num_c = int(input('Enter third side of a triangle: '))
+#
+# if input_num_a + input_num_b > input_num_c and input_num_b + input_num_c > input_num_a and input_num_c + input_num_a > input_num_b:
+#     print(True)
+# else:
+#     print(False)
+
 # TODO 2 task
 
 # input_num = int(input('Enter a number: '))
@@ -19,7 +28,7 @@
 
 # TODO 4 task
 
-# input_num_a = int(input('Enter first number: '))
-# input_num_b = int(input('Enter second number: '))
-#
-# print(input_num_a > input_num_b)
+input_num_a = int(input('Enter first number: '))
+input_num_b = int(input('Enter second number: '))
+
+print(input_num_a > input_num_b)
