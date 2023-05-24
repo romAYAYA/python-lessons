f99d16ef85969f8d5f1924b8e038cb1246819add
Roman Khabayev <supernaturalmlady@gmail.com>
2023-05-24 19:26:19 +0600

    classwork, 4th lesson, 1st task rewritten with while loop

--- _6_classworks/_4_lesson/_1_task.py
+++ _6_classworks/_4_lesson/_1_task.py
@@ -3,7 +3,13 @@ input_num_end = int(input('Enter number b: '))
 
 sum_of_evens = 0
 
-for i in range(input_num_begin, input_num_end + 1):
-    if i % 2 == 0:
-        sum_of_evens += i
+# for i in range(input_num_begin, input_num_end + 1):
+#     if i % 2 == 0:
+#         sum_of_evens += i
+# print(sum_of_evens)
+
+while input_num_begin <= input_num_end:
+    if input_num_begin % 2 == 0:
+        sum_of_evens += input_num_begin
+    input_num_begin += 1
 print(sum_of_evens)
