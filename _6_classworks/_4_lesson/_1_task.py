e6b16d29947ef0f29b62d508dc7c159bbd8283eb
Roman Khabayev <supernaturalmlady@gmail.com>
2023-05-24 19:14:22 +0600

    classwork, 1st task, 4th lesson completed

---
+++ _6_classworks/_4_lesson/_1_task.py
@@ -0,0 +1,9 @@
+input_num_begin = int(input('Enter number a: '))
+input_num_end = int(input('Enter number b: '))
+
+sum_of_evens = 0
+
+for i in range(input_num_begin, input_num_end + 1):
+    if i % 2 == 0:
+        sum_of_evens += i
+print(sum_of_evens)
