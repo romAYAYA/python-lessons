a5434ebd1b7803d218cdc1d3b28203b7f940c4f2
Roman Khabayev <supernaturalmlady@gmail.com>
2023-06-04 22:35:23 +0600

    hw 5 fourth and fifth tasks completed. Hw done

--- _7_homeworks/_5_hw/main.py
+++ _7_homeworks/_5_hw/main.py
@@ -41,14 +41,57 @@
 
 # TODO task 3
 
-def sqrt(num: int) -> list:
-    res = []
+# def sqrt(num: int) -> list:
+#     res = []
+#
+#     for i in range(1, num):
+#         if i ** 2 < num:
+#             res.append(i ** 2)
+#
+#     return res
+#
+#
+# print(sqrt(50))
 
-    for i in range(1, num):
-        if i ** 2 < num:
-            res.append(i ** 2)
+# TODO task 4
 
-    return res
+# def calc(num_1: float, num_2: float, action: str) -> float | str:
+#     match action:
+#         case '+':
+#             res = num_1 + num_2
+#             return res
+#         case '-':
+#             res = num_1 - num_2
+#             return res
+#         case '*':
+#             res = num_1 * num_2
+#             return res
+#         case '/':
+#             if num_2 != 0:
+#                 res = num_1 / num_2
+#                 return round(res, 1)
+#             else:
+#                 return 'You should not do this :c'
+#         case _:
+#             return 'Invalid action'
+#
+#
+# num_1_input = float(input('Enter first number: '))
+# num_2_input = float(input('Enter second number: '))
+# action_input = str(input('Enter an action (+, -, *, /): '))
+#
+# print(calc(num_1_input, num_2_input, action_input))
+
+# TODO 5 task
+
+def check_accessory(list_nums: list[int], check_nums: list[int]) -> bool:
+    if not list_nums:
+        return True
+    elif list_nums[0] not in check_nums:
+        return False
+    else:
+        return check_accessory(list_nums[1:], check_nums)
 
 
-print(sqrt(50))
+print(check_accessory([2, 2, 1, 2], [1, 2]))
+print(check_accessory([2, 2, 3, 2], [1, 2]))
