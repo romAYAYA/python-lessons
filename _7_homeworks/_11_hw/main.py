4a8f3d257195f68b3f954576636abb0217be3690
Roman Khabayev <supernaturalmlady@gmail.com>
2023-06-29 21:34:47 +0600

    hw 11 async function added

--- _7_homeworks/_11_hw/main.py
+++ _7_homeworks/_11_hw/main.py
@@ -4,13 +4,16 @@ import tkinter as tk
 from tkinter import messagebox
 import multiprocessing
 import threading
+import asyncio
+import aiohttp
 
 
-def download_image(image_url, image_path):
-    res = requests.get(image_url)
-    with open(image_path, 'wb') as file:
-        file.write(res.content)
+# TODO download func for sync, multiprocessing, threading
 
+# def download_image(image_url, image_path):
+#     res = requests.get(image_url)
+#     with open(image_path, 'wb') as file:
+#         file.write(res.content)
 
 # TODO sync
 
@@ -49,32 +52,65 @@ def download_image(image_url, image_path):
 
 # TODO threading
 
-def download_images_thread():
+# def download_images_thread():
+#     folder_path = './temp'
+#     os.makedirs(folder_path, exist_ok=True)
+#
+#     threads = []
+#     for i in range(1, 11):
+#         image_url = f'https://picsum.photos/500/500/?random={i}'
+#         image_path = os.path.join(folder_path, f'image_{i}.jpg')
+#
+#         thread = threading.Thread(target=download_image, args=(image_url, image_path))
+#         thread.start()
+#         threads.append(thread)
+#
+#     for thread in threads:
+#         thread.join()
+#
+#     messagebox.showinfo('Download Complete', 'Images downloaded with threading!')
+
+# TODO async
+
+async def download_image(session, image_url, image_path):
+    async with session.get(image_url) as res:
+        with open(image_path, 'wb') as file:
+            while True:
+                chunk = await res.content.read(1024)
+                if not chunk:
+                    break
+                file.write(chunk)
+
+
+async def download_images_async():
     folder_path = './temp'
     os.makedirs(folder_path, exist_ok=True)
 
-    threads = []
-    for i in range(1, 11):
-        image_url = f'https://picsum.photos/500/500/?random={i}'
-        image_path = os.path.join(folder_path, f'image_{i}.jpg')
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for i in range(1, 11):
+            image_url = f'https://picsum.photos/500/500/?random={i}'
+            image_path = os.path.join(folder_path, f'image_{i}.jpg')
+            tasks.append(download_image(session, image_url, image_path))
 
-        thread = threading.Thread(target=download_image, args=(image_url, image_path))
-        thread.start()
-        threads.append(thread)
+        await asyncio.gather(*tasks)
 
-    for thread in threads:
-        thread.join()
-
-    messagebox.showinfo('Download Complete', 'Images downloaded with threading!')
+    messagebox.showinfo('Download Complete', 'Images downloaded with asyncio!')
 
 
 window = tk.Tk()
 window.title('Image Downloader')
+window.geometry('300x90')
 
 if __name__ == '__main__':
+    # TODO asyncio
+    button = tk.Button(window, text='Download Images', command=lambda: asyncio.run(download_images_async()))
+
+    # TODO others
     # в command нужно изменить функцию, которая должна отработать (download_images_sync, download_images_multiprocess,
     # download_images_thread или асинхронная)
-    button = tk.Button(window, text='Download Images', command=download_images_thread)
+    # button = tk.Button(window, text='Download Images', command=download_images_sync)
+
     button.pack(padx=20, pady=10)
 
     window.mainloop()
