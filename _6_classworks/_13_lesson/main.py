0a12338dc8906122697c37383481d8b21876805f
Roman Khabayev <supernaturalmlady@gmail.com>
2023-08-23 20:12:34 +0600

    little changes, practice added

---
+++ _6_classworks/_13_lesson/main.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+# import json
+# import os
+#
+# app = tk.Tk()
+# app.title('Button clicked')
+#
+# label = tk.Label(app, text="Click the button")
+
+
+# def upload_json():
+#     dir_path = 'temp'
+#     completed_data = []
+#
+#     file_list = os.listdir(dir_path)
+#
+#     for filename in file_list:
+#         if filename.endswith('.json'):
+#             file_path = os.path.join(dir_path, filename)
+#             with open(file_path, 'r') as input_file:
+#                 data = json.load(input_file)
+#                 completed_data.append(data)
+#
+#     with open('new_json.json', 'w') as output_file:
+#         json.dump(completed_data, output_file, indent=4)
+#
+#     label.config(text="JSON uploaded from {} files".format(len(completed_data)))
+
+# def upload_json():
+#
+#
+# button = tk.Button(app, text="Click me", command=upload_json)
+#
+# label.pack()
+# button.pack()
+#
+# app.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
